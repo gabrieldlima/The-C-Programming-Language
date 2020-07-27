fc611f75726a532a2a601df27039b0bf522bc229
Gabriel Lima <gabrielgsdlima@gmail.com>
2020-07-27 15:07:33 -0300

    add resolution of exercise 2

---
+++ Chapter1-A-Tutorial-Introduction/exercise1-2.c
@@ -0,0 +1,14 @@
+/*
+|Exercise 1-2: Experiment to find out what happens when printf's argument      |
+|string contains \c, where c is some character not listed above.               |
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+    printf("Hello, world\n");
+
+    return 0;
+}
