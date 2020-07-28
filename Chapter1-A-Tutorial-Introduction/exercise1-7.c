2fca9d092e5f80b44d61c4ee6b7cec1e7b0c9840
Gabriel Lima <gabrielgsdlima@gmail.com>
2020-07-28 13:16:43 -0300

    add resolution of exercise 7

---
+++ Chapter1-A-Tutorial-Introduction/exercise1-7.c
@@ -0,0 +1,13 @@
+/*
+|Exercise 1-7: Write a program to print the value of EOF.                      |
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+    printf("EOF is %d\n", EOF);
+
+    return 0;
+}
