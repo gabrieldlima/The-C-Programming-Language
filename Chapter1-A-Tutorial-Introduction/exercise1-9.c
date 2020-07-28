46380aaa67b2679f3bd19cc627818ec5da9e2fc8
Gabriel Lima <gabrielgsdlima@gmail.com>
2020-07-28 14:31:06 -0300

    add resolution of exercise 9

---
+++ Chapter1-A-Tutorial-Introduction/exercise1-9.c
@@ -0,0 +1,34 @@
+/*
+|Exercise 1-9: Write a program to copy its input to its output, replacing each |
+|string of one or more blanks by a single blank.                               |
+*/
+
+#include <stdio.h>
+
+#define NOBLANK 'a'
+
+int main(void)
+{
+    int c, lastc;
+
+    lastc = NOBLANK;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+            putchar(c);
+        }
+        if (c == ' ')
+        {
+            if (lastc != ' ')
+            {
+                putchar(c);
+            }
+        }
+        
+        lastc = c;
+    }
+
+    return 0;
+}
